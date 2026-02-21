7b52812742bcd156987c51c61ab3c7896b42658f
tsp0602 <161592035+tsp0602@users.noreply.github.com>
2026-02-21 23:11:10 +0530

    feat: add star and number pattern programs (square, triangle, pyramid)

---
+++ list.py
@@ -0,0 +1,71 @@
+# ⭐ Q1 — Square Star Pattern (Easy)
+# ****
+# ****
+# ****
+# ****
+
+# n=int(input("Enter number of lines: "))
+# for i in range (1,n+1):
+#     for j in range(1,n+1):
+#         print(" * ",end="")
+#     print()
+
+
+
+# -------------------------------------------------------------------------------------------
+# ⭐ Q2 — Right Triangle Pattern
+# *
+# **
+# ***
+# ****
+
+# n=int(input("Enter number of lines : "))
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print(" * ",end="")
+#     print()
+
+
+
+
+
+# -------------------------------------------------------------------------------------------
+# ⭐ Q4 — Number Triangle
+
+# 1
+# 12
+# 123
+# 1234
+
+# n=int(input("Enter number of lines : "))
+# for i in range (1,n+1):
+#     for j in range (1,i+1):
+#         print(j,end="")
+#     print()
+
+
+
+
+
+
+# -------------------------------------------------------------------------------------------
+# ⭐ Q5 — Pyramid Pattern (Very Important)
+
+
+#    *
+#   ***
+#  *****
+# *******
+
+# n = int(input("Enter number of line : "))
+# for i in range( 1,n+1):
+#     for j in range (1,n+i):
+#         if j<=n-i:
+#             print(" ",end="")
+#         else:
+#             print("*",end="")
+#     print()
+
+
+
+# -------------------------------------------------------------------------------------------
